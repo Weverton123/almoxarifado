97718d9ec49faf6633837c559566d21068b0c6eb
itxinho <italopithu@hotmail.com>
2013-09-02 16:39:41 -0300

    Editar e deletar usuario pronto!

---
+++ app/view/includes/deletarusu.php
@@ -0,0 +1,28 @@
+<?php if(!defined('BASEPATH')) exit('Falha no carregamento do BASEPATH!');
+  //Ativa o Buffer que armazena o conteúdo principal da página
+seguranca_arq();  
+ob_start();
+
+session_start();
+
+  if(!isset($_SESSION['session']['acoes']['idusuario'])){
+      redirecionar('?action=cliente');
+  }
+  
+   $usu = $_SESSION['session']['acoes']['idusuario'];
+   
+   
+  
+   $usuperm = new permissaoDAO();
+   $usuario = new usuarioDAO();
+   $usuperm->Deletar($usu);//primeiro é preciso excluir as permissões
+   $ret =  $usuario->Deletar($usu);//segundo exclui o usuario
+   
+   if($ret > 0){
+      $_SESSION['session']['acoes']['msg']='Usuário excluído com sucesso!';
+   }
+   else{
+      $_SESSION['session']['acoes']['msg']='Falha ao tentar excluir usuário!';  
+   }
+    
+  redirecionar('?action=cliente');
