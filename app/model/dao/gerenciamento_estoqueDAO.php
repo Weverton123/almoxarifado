1847236ca0cac95802f7c66b18f2b40be1ee03f8
itxinho <italopithu@hotmail.com>
2013-11-06 19:00:49 -0200

    #4 Criados arquivos Class e DAO

---
+++ app/model/dao/gerenciamento_estoqueDAO.php
@@ -0,0 +1,16 @@
+<?php if(!defined('BASEPATH')) exit(header('Location: ./../../../index.php'));
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of gerenciamento_estoqueDAO
+ *
+ * @author italo
+ */
+class gerenciamento_estoqueDAO {
+    //put your code here
+}
+
